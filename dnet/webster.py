4f32533dd85cb74b835f2d15e0c6c3f24335fcf8
David McClure <davidwilliammcclure@gmail.com>
2014-09-20 19:51:40 -0700

    Matching words.

--- dnet/webster.py
+++ dnet/webster.py
@@ -1,4 +1,33 @@
 
 
+import os
+import re
+
+
 class Webster(object):
-    pass
+
+
+    def __init__(self):
+
+        """
+        Open the dictionary file.
+        """
+
+        # Get the dictionary file path.
+        dirname = os.path.dirname(os.path.realpath(__file__))
+        webster = os.path.join(dirname, 'webster.txt')
+
+        # Read in the raw text content.
+        self.raw = open(webster, 'r').read()
+        self.index()
+
+
+    def index(self):
+
+        """
+        Build out a word -> definition(s) dictionary.
+        """
+
+        words = re.compile('\n[A-Z]+\n')
+        for match in re.finditer(words, self.raw):
+            print match.group(0)
