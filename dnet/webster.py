cedcf0fdf54d41432ba11c074e894bf2e692cc7c
David McClure <davidwilliammcclure@gmail.com>
2014-09-21 18:04:53 -0700

    Adds a basic network builder method.

--- dnet/webster.py
+++ dnet/webster.py
@@ -7,6 +7,7 @@ import enchant
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from collections import OrderedDict
+import networkx as nx
 
 
 class Webster(object):
@@ -57,8 +58,31 @@ class Webster(object):
             if not dictionary.check(word): continue
 
             tokens = []
+
+            # Filter out stopwords / noise.
             for t in re.findall('[a-z]+', defn):
                 if dictionary.check(t) and t not in estopwords:
                     tokens.append(self.stem(t))
 
             self.words[self.stem(word)] = tokens
+
+
+    def network(self):
+
+        """
+        Generate a network from the term -> siblings dictionary.
+        """
+
+        graph = nx.Graph()
+
+        for term, siblings in self.words.iteritems():
+            for sibling in siblings:
+
+                # Add edge if it doesn't exist.
+                if not graph.has_edge(term, sibling):
+                    graph.add_edge(term, sibling, weight=1)
+
+                # Otherwise, increment the weight.
+                else: graph[term][sibling]['weight'] += 1
+
+        return graph
